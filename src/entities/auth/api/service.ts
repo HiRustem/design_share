14dff24f1fe6a8c9d6504823682fcd6b490d5a13
Rustem <rustem_hajrullin@enot.work>
2025-09-03 07:34:20 +0300

    fix: login and register routes

--- src/entities/auth/api/service.ts
+++ src/entities/auth/api/service.ts
@@ -1,9 +1,10 @@
 import { authApi } from '@/shared/api';
 import { IAuthDto, IAuthResponse, IRegisterDto } from '../model/types';
 
-const login = (dto: IAuthDto) => authApi.post<IAuthResponse, IAuthDto>('/login', dto);
+const login = (dto: IAuthDto) => authApi.post<IAuthResponse, IAuthDto>('/auth/login', dto);
 
-const register = (dto: IRegisterDto) => authApi.post<IAuthResponse, IRegisterDto>('/register', dto);
+const register = (dto: IRegisterDto) =>
+  authApi.post<IAuthResponse, IRegisterDto>('/auth/register', dto);
 
 const authService = {
   login,
